9b5e2ea06d653fc14a6182c79fb2c38732869b95
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-03-29 16:56:21 +0900

    css loader

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import './popup.css';
+import './options.css';
 
 const domNode = document.createElement('div');
 domNode.id = 'root';
